5b6f81a9d8606b632261df27f0b6c5090ba0749a
Joel Lacey <joellaceybusiness@gmail.com>
2020-07-21 17:34:46 -0400

    Add basic leafly map integration

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
 import './styles.less';
+import mapImage from './assets/01_1920x1080_Map.jpg';
+
+import {
+    map
+} from 'leaflet';
+
+const mapid = L.map('map', {
+    crs: L.CRS.Simple,
+    zoomControl: false,
+    scrollWheelZoom: false
+});
+
+var bounds = [
+    [0, 0],
+    [800, 1500]
+];
+var image = L.imageOverlay(mapImage, bounds).addTo(mapid);
+
+mapid.fitBounds(bounds);
+
+
+//should only have map scrolling available on certain width, use window? 
+//or should 
