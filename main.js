fe002eaea5a5ef2a4394c0976d3dac7a5bab008a
Joel Lacey <joellaceybusiness@gmail.com>
2020-07-22 13:59:05 -0400

    Add map loading and pin location

--- main.js
+++ main.js
@@ -13,11 +13,24 @@ const mapid = L.map('map', {
 
 var bounds = [
     [0, 0],
-    [800, 1500]
+    [800, 1400]
 ];
 var image = L.imageOverlay(mapImage, bounds).addTo(mapid);
 
 mapid.fitBounds(bounds);
+L.tileLayer('', { attribution: 'test map'}).addTo(mapid);
+mapid.setMaxBounds(new L.LatLngBounds([0, 0], [800, 1400]));
+
+var popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(mapid);
+}
+
+mapid.on('click', onMapClick);
 
 
 //should only have map scrolling available on certain width, use window? 
