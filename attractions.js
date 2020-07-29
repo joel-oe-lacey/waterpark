03e0bb4da32c8ff8e65474c193a78a39cd42c6a3
Joel Lacey <joellaceybusiness@gmail.com>
2020-07-28 20:31:13 -0400

    Create attractions array

---
+++ attractions.js
@@ -0,0 +1,35 @@
+export const attractions = [
+    {
+        attrType: 'toddlers',
+        name: 'Pirate Ship',
+        photoLink: '',
+        videoLink: '',
+        img: {
+                url: './assets/03_640x426_2.jpg',
+                alt: 'A waterslide with a yellow carousel pattern'
+        },
+        markerName: 'pirateShip'
+    },
+    {
+        attrType: 'toddlers',
+        name: 'Kids Slide',
+        photoLink: '',
+        videoLink: '',
+        img: {
+            url: './assets/03_640x426_2.jpg',
+            alt: 'A waterslide with a yellow carousel pattern'
+        },
+        markerName: 'toddlerSlide'
+    },
+    {
+        attrType: 'toddlers',
+        name: 'Kids Bowl',
+        photoLink: '',
+        videoLink: '',
+        img: {
+            url: './assets/03_640x426_2.jpg',
+            alt: 'A waterslide with a yellow carousel pattern'
+        },
+        markerName: 'toddlerBowl'
+    },
+]
