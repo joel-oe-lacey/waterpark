d1d0c8e513a0a8baf24e9096eb1be8f602e8ddbb
Joel Lacey <joellaceybusiness@gmail.com>
2020-07-30 14:50:37 -0400

    Add fancybox dependency

--- main.js
+++ main.js
@@ -195,3 +195,50 @@ const filterMap = e => {
 }
 
 $('.tab').on('click', filterMap);
+
+$('[data-fancybox="images"]').fancybox({
+    buttons: [
+        'slideShow',
+        'share',
+        'zoom',
+        'fullScreen',
+        'close'
+    ],
+    thumbs: {
+        autoStart: true
+    }
+});
+
+//open marker, create popup. 
+//need title, need image, type
+//pass object through as datapoint 
+//store everything in an imported object stored under a key of markerID 
+//then target that here
+//if certain properties are empty don't display on popup
+const generateTiles = attractions => {
+    return attractions.map(attr => {
+        const {
+            attrType,
+            name,
+            photoLink,
+            videoLink,
+            img,
+            markerName
+        } = attr;
+        const color = colorMap[attrType];
+
+        return (`
+        <section class="tile">
+            <section class="tile-picture" style="border-bottom: 2px solid ${color}">
+                <span class="tile-icon" role="img" aria-label="glyph" style="color:${color}">☆</span>
+                <img src='${img.url}' alt="${img.alt}">
+            </section>
+            <h3 class="tile-title" style="color:${color}">${name}</h3>
+            <section class="tile-buttons">
+                <button class="tile-button" data-marker=${markerName}>☆</button>
+                <button class="tile-button" data-photo-link=${photoLink}>☆</button>
+                <button class="tile-button" data-video-link=${videoLink}>☆</button>
+            </section>
+        </section>`)
+    })
+}
