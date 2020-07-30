23b814f60da0dea8fb1d144280314856535fe0ee
Joel Lacey <joellaceybusiness@gmail.com>
2020-07-30 14:49:19 -0400

    Restructure attractions object to sort by marker label

--- attractions.js
+++ attractions.js
@@ -1,9 +1,8 @@
 //Add rides to this list under their associated category, with associated details
 //markerName must match the name of the marker variable that is plotted on the map for correct targeting and linking
 
-export const attractionsByType = {
-    toddlers: [
-        {
+export const attractionsByMarker = {
+    pirateShip: {
             attrType: 'toddlers',
             name: 'Pirate Ship',
             photoLink: '',
@@ -13,7 +12,8 @@ export const attractionsByType = {
                 alt: 'A waterslide with a yellow carousel pattern'
             },
             markerName: 'pirateShip'
-        }, {
+        }, 
+    toddlerSlide: {
             attrType: 'toddlers',
             name: 'Kids Slide',
             photoLink: '',
@@ -23,7 +23,8 @@ export const attractionsByType = {
                 alt: 'A waterslide with a yellow carousel pattern'
             },
             markerName: 'toddlerSlide'
-        }, {
+        }, 
+    toddlerBowl: {
             attrType: 'toddlers',
             name: 'Kids Bowl',
             photoLink: '',
@@ -33,10 +34,8 @@ export const attractionsByType = {
                 alt: 'A waterslide with a yellow carousel pattern'
             },
             markerName: 'toddlerBowl'
-        }
-    ],
-    everyone: [
-        {
+    },
+    lazyRiver: {
             attrType: 'everyone',
             name: 'Lazy River',
             photoLink: '',
@@ -46,8 +45,8 @@ export const attractionsByType = {
                 alt: 'A waterslide with a yellow carousel pattern'
             },
             markerName: 'lazyRiver'
-        },
-        {
+    },
+    circus: {
             attrType: 'everyone',
             name: 'circus',
             photoLink: '',
@@ -57,8 +56,8 @@ export const attractionsByType = {
                 alt: 'A waterslide with a yellow carousel pattern'
             },
             markerName: 'circus'
-        },
-        {
+    },
+    pirateLagoon: {
             attrType: 'everyone',
             name: 'Pirate Lagoon',
             photoLink: '',
@@ -68,8 +67,8 @@ export const attractionsByType = {
                 alt: 'A waterslide with a yellow carousel pattern'
             },
             markerName: 'pirateLagoon'
-        },
-        {
+    },
+    smallLagoon: {
             attrType: 'everyone',
             name: 'Small Lagoon',
             photoLink: '',
@@ -79,8 +78,8 @@ export const attractionsByType = {
                 alt: 'A waterslide with a yellow carousel pattern'
             },
             markerName: 'smallLagoon'
-        },
-        {
+    },
+    wavepool: {
             attrType: 'everyone',
             name: 'Wavepool',
             photoLink: '',
@@ -90,8 +89,8 @@ export const attractionsByType = {
                 alt: 'A waterslide with a yellow carousel pattern'
             },
             markerName: 'wavepool'
-        },
-        {
+    },
+    beach: {
             attrType: 'everyone',
             name: 'Beach',
             photoLink: '',
@@ -101,8 +100,8 @@ export const attractionsByType = {
                 alt: 'A waterslide with a yellow carousel pattern'
             },
             markerName: 'beach'
-        },
-        {
+    },
+    flatSlide: {
             attrType: 'everyone',
             name: 'Long Slide',
             photoLink: '',
@@ -112,8 +111,8 @@ export const attractionsByType = {
                 alt: 'A waterslide with a yellow carousel pattern'
             },
             markerName: 'flatSlide'
-        },
-        {
+    },
+    windSlide: {
             attrType: 'everyone',
             name: 'Windy Slide',
             photoLink: '',
@@ -123,8 +122,8 @@ export const attractionsByType = {
                 alt: 'A waterslide with a yellow carousel pattern'
             },
             markerName: 'windSlide'
-        },
-        {
+    },
+    funnelShute: {
             attrType: 'everyone',
             name: 'Funnel Shute',
             photoLink: '',
@@ -134,6 +133,270 @@ export const attractionsByType = {
                 alt: 'A waterslide with a yellow carousel pattern'
             },
             markerName: 'funnelShute'
-        }
-    ]
+    },
+    spiralShute: {
+        attrType: 'everyone',
+        name: 'Spiral Shute',
+        photoLink: '',
+        videoLink: '',
+        img: {
+            url: './assets/02_640x426_1.jpg',
+            alt: 'A waterslide with a yellow carousel pattern'
+        },
+        markerName: 'spiralShute'
+    },
+    clamshell: {
+        attrType: 'everyone',
+        name: 'Clamshell',
+        photoLink: '',
+        videoLink: '',
+        img: {
+            url: './assets/02_640x426_1.jpg',
+            alt: 'A waterslide with a yellow carousel pattern'
+        },
+        markerName: 'clamshell'
+    },
+    funWall: {
+        attrType: 'everyone',
+        name: 'Fun Wall',
+        photoLink: '',
+        videoLink: '',
+        img: {
+            url: './assets/02_640x426_1.jpg',
+            alt: 'A waterslide with a yellow carousel pattern'
+        },
+        markerName: 'funWall'
+    },
+    lazyRiverSlide: {
+        attrType: 'everyone',
+        name: 'Lazy River Slide',
+        photoLink: '',
+        videoLink: '',
+        img: {
+            url: './assets/02_640x426_1.jpg',
+            alt: 'A waterslide with a yellow carousel pattern'
+        },
+        markerName: 'lazyRiverSlide'
+    },
+    cresentSlide: {
+        attrType: 'everyone',
+        name: 'Cresent Slide',
+        photoLink: '',
+        videoLink: '',
+        img: {
+            url: './assets/02_640x426_1.jpg',
+            alt: 'A waterslide with a yellow carousel pattern'
+        },
+        markerName: 'cresentSlide'
+    },
+    loopSlide: {
+        attrType: 'extreme',
+        name: 'Loop Slide',
+        photoLink: '',
+        videoLink: '',
+        img: {
+            url: './assets/02_640x426_1.jpg',
+            alt: 'A waterslide with a yellow carousel pattern'
+        },
+        markerName: 'loopSlide'
+    },
+    curvySlide: {
+        attrType: 'extreme',
+        name: 'Curvy Slide',
+        photoLink: '',
+        videoLink: '',
+        img: {
+            url: './assets/02_640x426_1.jpg',
+            alt: 'A waterslide with a yellow carousel pattern'
+        },
+        markerName: 'curvySlide'
+    },
+    speedSlide: {
+        attrType: 'extreme',
+        name: 'Speed Slide',
+        photoLink: '',
+        videoLink: '',
+        img: {
+            url: './assets/02_640x426_1.jpg',
+            alt: 'A waterslide with a yellow carousel pattern'
+        },
+        markerName: 'speedSlide'
+    },
+    bigSlide: {
+        attrType: 'extreme',
+        name: 'Big Slide',
+        photoLink: '',
+        videoLink: '',
+        img: {
+            url: './assets/02_640x426_1.jpg',
+            alt: 'A waterslide with a yellow carousel pattern'
+        },
+        markerName: 'bigSlide'
+    },
+    carouselSlide: {
+        attrType: 'extreme',
+        name: 'Carousel Slide',
+        photoLink: '',
+        videoLink: '',
+        img: {
+            url: './assets/02_640x426_1.jpg',
+            alt: 'A waterslide with a yellow carousel pattern'
+        },
+        markerName: 'carouselSlide'
+    },
+    nwRestroom: {
+        attrType: 'wc',
+        name: 'Northwest WC',
+        photoLink: '',
+        videoLink: '',
+        img: {
+            url: './assets/02_640x426_1.jpg',
+            alt: 'A waterslide with a yellow carousel pattern'
+        },
+        markerName: 'nwRestroom'
+    },
+    pirateRestroom: {
+        attrType: 'wc',
+        name: 'Pirate WC',
+        photoLink: '',
+        videoLink: '',
+        img: {
+            url: './assets/02_640x426_1.jpg',
+            alt: 'A waterslide with a yellow carousel pattern'
+        },
+        markerName: 'pirateRestroom'
+    },
+    beachRestroom: {
+        attrType: 'wc',
+        name: 'Beach WC',
+        photoLink: '',
+        videoLink: '',
+        img: {
+            url: './assets/02_640x426_1.jpg',
+            alt: 'A waterslide with a yellow carousel pattern'
+        },
+        markerName: 'beachRestroom'
+    },
+    centralRestroom: {
+        attrType: 'wc',
+        name: 'Central WC',
+        photoLink: '',
+        videoLink: '',
+        img: {
+            url: './assets/02_640x426_1.jpg',
+            alt: 'A waterslide with a yellow carousel pattern'
+        },
+        markerName: 'centralRestroom'
+    },
+    mainSWRestroom: {
+        attrType: 'wc',
+        name: 'Main Southwest WC',
+        photoLink: '',
+        videoLink: '',
+        img: {
+            url: './assets/02_640x426_1.jpg',
+            alt: 'A waterslide with a yellow carousel pattern'
+        },
+        markerName: 'mainSWRestroom'
+    },
+    mainSERestroom: {
+        attrType: 'wc',
+        name: 'Main Southeast WC',
+        photoLink: '',
+        videoLink: '',
+        img: {
+            url: './assets/02_640x426_1.jpg',
+            alt: 'A waterslide with a yellow carousel pattern'
+        },
+        markerName: 'mainSERestroom'
+    },
+    mainSERestroom: {
+        attrType: 'wc',
+        name: 'Main Southeast WC',
+        photoLink: '',
+        videoLink: '',
+        img: {
+            url: './assets/02_640x426_1.jpg',
+            alt: 'A waterslide with a yellow carousel pattern'
+        },
+        markerName: 'mainSERestroom'
+    },
+    NWLockers: {
+        attrType: 'lockers',
+        name: 'Northwest Lockers',
+        photoLink: '',
+        videoLink: '',
+        img: {
+            url: './assets/02_640x426_1.jpg',
+            alt: 'A waterslide with a yellow carousel pattern'
+        },
+        markerName: 'NWLockers'
+    },
+    SELockers: {
+        attrType: 'lockers',
+        name: 'Southeast Lockers',
+        photoLink: '',
+        videoLink: '',
+        img: {
+            url: './assets/02_640x426_1.jpg',
+            alt: 'A waterslide with a yellow carousel pattern'
+        },
+        markerName: 'SELockers'
+    },
+    centralFood: {
+        attrType: 'food',
+        name: 'Central Restaurant',
+        photoLink: '',
+        videoLink: '',
+        img: {
+            url: './assets/02_640x426_1.jpg',
+            alt: 'A waterslide with a yellow carousel pattern'
+        },
+        markerName: 'centralFood'
+    },
+    beachFood: {
+        attrType: 'food',
+        name: 'Beach Restaurant',
+        photoLink: '',
+        videoLink: '',
+        img: {
+            url: './assets/02_640x426_1.jpg',
+            alt: 'A waterslide with a yellow carousel pattern'
+        },
+        markerName: 'beachFood'
+    },
+    mainFood: {
+        attrType: 'food',
+        name: 'Main Restaurant',
+        photoLink: '',
+        videoLink: '',
+        img: {
+            url: './assets/02_640x426_1.jpg',
+            alt: 'A waterslide with a yellow carousel pattern'
+        },
+        markerName: 'mainFood'
+    },
+    mainParking: {
+        attrType: 'parking',
+        name: 'Main Parking',
+        photoLink: '',
+        videoLink: '',
+        img: {
+            url: './assets/02_640x426_1.jpg',
+            alt: 'A waterslide with a yellow carousel pattern'
+        },
+        markerName: 'mainParking'
+    },
+    mainAid: {
+        attrType: 'aid',
+        name: 'First Aid',
+        photoLink: '',
+        videoLink: '',
+        img: {
+            url: './assets/05_640x426_4.jpg',
+            alt: 'A bright red rescue flotation ring'
+        },
+        markerName: 'mainAid'
+    },
+
 }
